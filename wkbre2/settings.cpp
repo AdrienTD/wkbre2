24ea8f48a6a40f4dd803ee6ef68fefee7b0cc914
AdrienTD <agpetit69@gmail.com>
2025-11-29 18:22:16 +0100

    Create default wkconfig.json if not present

--- wkbre2/settings.cpp
+++ wkbre2/settings.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <codecvt>
 #include <locale>
+#include <filesystem>
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
@@ -15,8 +16,38 @@
 
 nlohmann::json g_settings;
 
+static const std::filesystem::path configPath = "wkconfig.json";
+
+static void ResetDefaultSettings()
+{
+	g_settings["game_path"] = ".";
+	g_settings["gameVersion"] = 0;
+	g_settings["musicEnabled"] = true;
+#ifdef _WIN32
+	g_settings["renderer"] = "d3d11";
+	g_settings["msaaNumSamples"] = 1;
+	g_settings["d3d11AdapterIndex"] = 0;
+	g_settings["enhancedGraphics"] = true;
+#endif
+}
+
+static void WriteSettings()
+{
+	std::ofstream file(configPath);
+	if (file.good()) {
+		file << g_settings.dump(4);
+	}
+}
+
 void LoadSettings() {
-	std::ifstream file("wkconfig.json");
+
+	if (!std::filesystem::exists(configPath)) {
+		ResetDefaultSettings();
+		WriteSettings();
+		return;
+	}
+
+	std::ifstream file(configPath);
 	if (file.good()) {
 		try {
 			file >> g_settings;
