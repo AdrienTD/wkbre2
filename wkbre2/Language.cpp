a21f7c115dbcf9c5a5ac63518fd2369b256f7fe5
AdrienTD <agpetit69@gmail.com>
2025-12-07 15:57:50 +0100

    Language: Add Latin1 to UTF8 conversion

--- wkbre2/Language.cpp
+++ wkbre2/Language.cpp
@@ -6,6 +6,24 @@
 #include "file.h"
 #include "util/GSFileParser.h"
 
+namespace
+{
+	std::string convertLatin1ToUtf8(const std::string& str)
+	{
+		std::string u8str;
+		u8str.reserve(str.size());
+		for (char ch : str) {
+			unsigned char code = (unsigned char)ch;
+			if (code & 0x80) {
+				u8str.push_back((code >> 6) | 0b1100'0000);
+				u8str.push_back((code & 0b0011'1111) | 0b1000'0000);
+			}
+			else u8str.push_back(code);
+		}
+		return u8str;
+	}
+}
+
 void Language::load(const char* path)
 {
 	char* out; int outsize;
@@ -15,7 +33,7 @@ void Language::load(const char* path)
 	while (!gsf.eof) {
 		auto tag = gsf.nextTag();
 		auto str = gsf.nextString(true);
-		text[tag] = str;
+		text[tag] = convertLatin1ToUtf8(str);
 		gsf.advanceLine();
 	}
 	free(out);
